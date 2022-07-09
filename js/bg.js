9976c12a6ed007a26f7102f7171e9613891aae44
yang-sir-one <1964980878@qq.com>
2022-07-09 21:40:42 +0800

    Site updated: 2022-07-09 21:40:41

---
+++ js/bg.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-08 09:35:12
+ * @LastEditTime: 2022-03-08 09:35:39
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \Vue jsd:\BLOG\hexoblog\themes\butterfly\source\js\bg.js
+ */
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+    full_page[0].style.background = "transparent";
+}
